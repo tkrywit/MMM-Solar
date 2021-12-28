3d4519cad6078a7905a4389830d02963ad072245
Peter Backlund <peter@backlund.co>
2021-12-28 13:03:45 -0500

    Replaced requests module with https

--- node_helper.js
+++ node_helper.js
@@ -1,26 +1,31 @@
-
-var request = require('request');
+var https = require("https");
 var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
+  start: function () {
+    console.log("Starting node helper: " + this.name);
+  },
 
-	start: function() {
-		console.log("Starting node helper: " + this.name);
+  socketNotificationReceived: function (notification, payload) {
+    var self = this;
+    console.log("Notification: " + notification + " Payload: " + payload);
 
-	},
-
-	socketNotificationReceived: function(notification, payload) {
-		var self = this;
-		console.log("Notification: " + notification + " Payload: " + payload);
-
-		if(notification === "GET_SOLAR") {
-			var enlightenUrl = payload.config.url + payload.config.systemId + "/summary?&key=" + payload.config.apiKey + "&user_id=" + payload.config.userId;
-			request(enlightenUrl, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					var jsonData = JSON.parse(body);
-				        self.sendSocketNotification("SOLAR_DATA", jsonData);
-				}
-			});
-		}
-	},
+    if (notification === "GET_SOLAR") {
+      var enlightenUrl =
+        payload.config.url +
+        payload.config.systemId +
+        "/summary?&key=" +
+        payload.config.apiKey +
+        "&user_id=" +
+        payload.config.userId;
+      https.get(enlightenUrl, function (response) {
+        if (response.statusCode == 200) {
+          response.on("data", function (body) {
+            var jsonData = JSON.parse(body);
+            self.sendSocketNotification("SOLAR_DATA", jsonData);
+          });
+        }
+      });
+    }
+  },
 });
